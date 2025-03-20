117703554d18decb2facbf2c93be59be3470a5b3
Anjans1991 <anjansv1991l.com>
2025-03-20 23:24:25 +0530

    updated changes$ git config --global user.name

---
+++ Hello.py
@@ -0,0 +1,18 @@
+print ("Srushti anjan")
+a=2
+b=3
+c=a+b
+print(c)
+print(a+b)
+print(a<b)
+
+
+age=21
+if age>=20:
+    print("please enter site")
+else:
+    print("No entry")
+
+    for i in range(2):  
+        print(i)
+
